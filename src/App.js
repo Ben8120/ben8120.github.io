31eeafe6484902b8cd687ea7fd2444f162b7d494
Ben <chuabenshern5@gmail.com>
2023-01-14 12:29:25 +0800

    testing full window section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>This website is undergoing construction</h1>
-        <p>Please come back tomorrow :)</p>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <h1>This website is undergoing construction</h1>
+          <p>Please come back tomorrow :)</p>
+        </header>
+      </div>
+      <div className='App Section'>
+        <p>test</p>
+      </div>
+      <div className='App Section' style={{background:"red"}}>
+        <p>test 2</p>
+      </div>
     </div>
   );
 }
